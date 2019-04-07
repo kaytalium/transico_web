e9f6afd7477186aff547ce2c938ba91cfb78392f
Ovel Heslop <kayheslop@gmail.com>
2019-04-06 23:28:15 -0500

    Add google maps to project

--- src/app/@application/vehicle-manager/vehicle-manager.module.ts
+++ src/app/@application/vehicle-manager/vehicle-manager.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehicleManagerComponent } from './vehicle-manager/vehicle-manager.component';
 import { NavigationTagModule } from '@src/app/@component/navigation-tag';
+import { AgmCoreModule } from '@agm/core'
+import { AgmDirectionModule } from 'agm-direction'
 
 @NgModule({
   declarations: [VehicleManagerComponent],
   imports: [
     CommonModule,
-    NavigationTagModule
+    NavigationTagModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA1f1qwbe1HEFwmc-vdl-l8mIaNIPu_M_w'
+    }),
+    AgmDirectionModule
   ],
   exports:[
     VehicleManagerComponent
--- src/app/@application/vehicle-manager/vehicle-manager/vehicle-manager.component.ts
+++ src/app/@application/vehicle-manager/vehicle-manager/vehicle-manager.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-vehicle-manager',
-  templateUrl: './vehicle-manager.component.html',
-  styleUrls: ['./vehicle-manager.component.css']
+  selector: "app-vehicle-manager",
+  templateUrl: "./vehicle-manager.component.html",
+  styleUrls: ["./vehicle-manager.component.css"]
 })
 export class VehicleManagerComponent implements OnInit {
+  // AIzaSyAvUGU_C2t2yVlLMC5VFNswzfjuLB2kw6M
 
-  constructor() { }
+  dir: Object;
+  public waypoints: any = [];
+  public renderOptions = {
+    draggable: true
+  };
+  
+
+  constructor() {}
 
   ngOnInit() {
+    this.dir = {
+      origin: { lat: 17.973379, lng: -76.758659 },
+      destination: { lat: 17.98637, lng: -76.94783 }
+    };
   }
 
+  public change(event: any) {
+    this.waypoints = event.request.waypoints;
+  }
 }
